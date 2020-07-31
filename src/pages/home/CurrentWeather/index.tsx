af036afb85fdd1ca509f9469b544b4fa44f1b915
Brian West <brianwilliamwest@gmail.com>
2020-07-31 16:11:04 -0400

    feat: add json-server

--- src/pages/home/CurrentWeather/index.tsx
+++ src/pages/home/CurrentWeather/index.tsx
@@ -1,18 +1,31 @@
 import React, { FC } from 'react'
 import { MetaText } from 'components/text/MetaText'
 
-export const CurrentWeather: FC = () => {
+interface CurrentWeatherProps {
+  currentTemp: number
+  feelsLikeTemp: number
+  location?: string
+  sunset: string
+}
+
+export const CurrentWeather: FC<CurrentWeatherProps> = ({
+  currentTemp,
+  feelsLikeTemp,
+  sunset,
+}) => {
   return (
     <div className={'space-y-1 text-center'}>
       <div className={'flex flex-row align-top space-x-2 justify-center'}>
-        <span className={'text-6xl'}>28</span>
-        <span className={'text-2xl text-gray-400 font-light py-4'}>°C</span>
+        <span className={'text-6xl'}>{currentTemp}</span>
+        <span className={'text-2xl text-gray-400 font-light py-4'}>°F</span>
       </div>
       <div>
         <MetaText>Barcelona, Spain</MetaText>
       </div>
       <div>
-        <MetaText>Feels like 22 • Sunset 20:15</MetaText>
+        <MetaText>
+          Feels like {feelsLikeTemp} • Sunset {sunset}
+        </MetaText>
       </div>
     </div>
   )
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,36 @@
 import React, { FC } from 'react'
 import { DateInfo } from './DateInfo'
 import { CurrentWeather } from './CurrentWeather'
+import { useQuery } from 'react-query'
+import { CurrentWeatherResponse } from 'types'
+import { format } from 'date-fns'
+
+function kelvinToF(kelvin: number) {
+  const celsius = kelvin - 273
+  return Math.floor(celsius * (9 / 5) + 32)
+}
 
 export const HomePage: FC = () => {
+  const { data } = useQuery<CurrentWeatherResponse, string>(
+    'currentWeather',
+    () => fetch(`http://localhost:3001/weather`).then((res) => res.json())
+  )
+
+  if (!data) {
+    return null
+  }
+
   return (
     <div className={'h-screen bg-blue-900 p-4 space-y-4 text-white'}>
       <div className={'text-center'}>
         <p className={'text-gray-200'}>Weather Forecast</p>
       </div>
       <DateInfo />
-      <CurrentWeather />
+      <CurrentWeather
+        currentTemp={kelvinToF(data.main.temp)}
+        feelsLikeTemp={kelvinToF(data.main.feels_like)}
+        sunset={format(data.sys.sunset, 'hh:mm a')}
+      />
     </div>
   )
 }
